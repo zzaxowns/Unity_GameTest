693075a0ef773f8118037c78b75e718a2e7172ef
KimTaeJun <zzaxowns@naver.com>
2020-05-01 15:41:34 +0900

    first commit
    1 commit

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Music : MonoBehaviour
+{
+
+    public Slider bGM;
+    private AudioSource source;
+
+    private float bgm_Vol = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.source = this.gameObject.GetComponent<AudioSource>();
+        source.volume = bgm_Vol;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source.volume = bGM.value;
+        bgm_Vol = bGM.value;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject PlayerState;
+    public GameObject OptionState;
+
+    public Text axText;
+    public Text armorText;
+    public Text helmetText;
+    public Text MPText;
+    public Text HpText;
+    public Text ringText;
+
+    int axState;
+    int armorState;
+    int helmetState;
+    int mp;
+    int hp;
+    int ringState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void randinit() {
+        axState = Random.Range(10, 30);
+        armorState = Random.Range(20, 40);
+        helmetState = Random.Range(10, 20);
+        mp = Random.Range(50, 70);
+        hp = Random.Range(90, 100);
+        ringState = Random.Range(10, 40);
+
+
+        axText.text = axState.ToString();
+        armorText.text = armorState.ToString();
+        helmetText.text = helmetState.ToString();
+        MPText.text = mp.ToString();
+        HpText.text = hp.ToString();
+        ringText.text = ringState.ToString();
+    }
+
+    public void popState() {
+        PlayerState.SetActive(true);
+    }
+
+    public void closeState() {
+        PlayerState.SetActive(false);
+    }
+
+    public void popOption() {
+        OptionState.SetActive(true);
+    }
+
+    public void closeOption() {
+        OptionState.SetActive(false);
+    }
+
+}
